e889f682136ad966b22c0b400409ab7ba6d9012e
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-06-10 21:03:53 -0300

    API response/send

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const countriesContainer = document.querySelector(".countries");
 const request = new XMLHttpRequest();
 request.open("GET", "https://restcountries.com/v2/name/brazil");
 request.send();
+
+request.addEventListener("load", function () {
+  const [data] = JSON.parse(this.responseText);
+});
