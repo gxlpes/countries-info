6ac03fc5618b69b32cd36044264469633b19b3a7
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-06-10 21:05:40 -0300

    DOM manipulation with API

--- script.js
+++ script.js
@@ -8,4 +8,16 @@ request.send();
 
 request.addEventListener("load", function () {
   const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  const html = ` <!-- <article class="country">
+  <img src="" alt="" class="country-img" />
+  <div class="country-data">
+    <h3 class="country-name"></h3>
+    <h4 class="country-region"></h4>
+    <p class="country-population"></p>
+    <p class="country-language"></p>
+    <p class="country-currency"></p>
+  </div>
+</article>`;
 });
